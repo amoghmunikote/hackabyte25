af269e50d261607fae7859a19fd6fa22f1218e8b
Amogh Munikote <amogh.munikote@gmail.com>
2025-03-30 10:09:45 -0700

    Added transition to next screeen

--- app/src/main/java/com/example/hackabyte25/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/hackabyte25/ui/slideshow/SlideshowFragment.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.hackabyte25.R;
 import com.example.hackabyte25.databinding.FragmentSlideshowBinding;
+import com.example.hackabyte25.ui.editmenu.EditMenuFragment;
 
 public class SlideshowFragment extends Fragment {
 
@@ -23,6 +27,25 @@ public class SlideshowFragment extends Fragment {
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button editMenuButton = binding.editMenuButton;
+        editMenuButton.setOnClickListener(v -> {
+            EditMenuFragment editMenuFragment = new EditMenuFragment();
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            // Set transition animation
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
+            // Replace current fragment and add to back stack
+            transaction.replace(R.id.nav_host_fragment_content_main, editMenuFragment);
+            transaction.addToBackStack(null);
+
+            // Hide current fragment's view
+            binding.getRoot().setVisibility(View.GONE);
+
+            transaction.commit();
+        });
 
         return root;
     }
